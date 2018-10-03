ec76bf3b6062a25b550f32ffaef6cdc31976a51c
Nazmi <nazmifeeroz@hotmail.com>
2018-10-03 22:15:00 +0800

    finished building server side

--- resolvers/index.js
+++ resolvers/index.js
@@ -86,13 +86,39 @@ module.exports = {
     me: (parent, args, { currentUser }) => currentUser
   },
   Mutation: {
-    postPhoto(parent, args) {
-      let newPhoto = {
-        id: _id++,
+    fakeUserAuth: async (parent, { githubLogin }, { db }) => {
+      const user = await db.collection("users").findOne({ githubLogin });
+      if (!user) {
+        throw new Error(`Cannot find user with githubLogin ${githubLogin}`);
+      }
+      return {
+        token: user.githubToken,
+        user
+      };
+    },
+    addFakeUsers: async (root, { count }, { db }) => {
+      const randomUserApi = `https://randomuser.me/api/?results=${count}`;
+      const { results } = await fetch(randomUserApi).then(res => res.json());
+      const users = results.map(r => ({
+        githubLogin: r.login.username,
+        name: `${r.name.first} ${r.name.last}`,
+        avatar: r.picture.thumbnail,
+        githubToken: r.login.sha1
+      }));
+      await db.collection("users").insert(users);
+      return users;
+    },
+    postPhoto: async (parent, args, { db, currentUser }) => {
+      if (!currentUser) {
+        throw new Error("only an authorized user can post a photo");
+      }
+      const newPhoto = {
         ...args.input,
+        userID: currentUser.githubLogin,
         created: new Date()
       };
-      photos.push(newPhoto);
+      const { insertedIds } = await db.collection("photos").insert(newPhoto);
+      newPhoto.id = insertedIds[0];
       return newPhoto;
     },
     async githubAuth(parent, { code }, { db }) {
@@ -129,9 +155,10 @@ module.exports = {
     }
   },
   Photo: {
-    url: parent => `test.com/${parent.id}.jpg`,
-    postedBy: parent => {
-      return users.find(u => u.githubLogin === parent.githubUser);
+    id: parent => parent.id || parent._id,
+    url: parent => `/img/photos/${parent.id}.jpg`,
+    postedBy: (parent, args, { db }) => {
+      return db.collection("users").findOne({ githubLogin: parent.userID });
     },
     taggedUsers: parent =>
       tags
