38c28019f6cc6480632d5dd78538b0309c9c93e1
Nazmi <nazmifeeroz@hotmail.com>
2018-10-04 20:52:08 +0800

    add delete collections

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,45 +1,6 @@
 const { GraphQLScalarType } = require("graphql");
 const fetch = require("node-fetch");
 
-// let _id = 0;
-// const photos = [
-//   {
-//     id: "1",
-//     name: "Dropping the Heart Chute",
-//     description: "The heart chute is one of my favorite chutes",
-//     category: "ACTION",
-//     githubUser: "gPlake",
-//     created: "3-28-1977"
-//   },
-//   {
-//     id: "2",
-//     name: "Enjoying the sunshine",
-//     category: "SELFIE",
-//     githubUser: "mHattrup",
-//     created: "1-2-1985"
-//   },
-//   {
-//     id: "3",
-//     name: "Gunbarrel 25",
-//     description: "25 laps on gunbarrel today",
-//     category: "LANDSCAPE",
-//     githubUser: "sSchmidt",
-//     created: "2018-04-15T19:09:57.308z"
-//   }
-// ];
-
-// const users = [
-//   { githubLogin: "mHattrup", name: "Mike Hattrup" },
-//   { githubLogin: "gPlake", name: "Glen Plake" },
-//   { githubLogin: "sSchmidt", name: "Scot Schmidt" }
-// ];
-
-// const tags = [
-//   { photoID: "1", userID: "gPlake" },
-//   { photoID: "1", userID: "mHattrup" },
-//   { photoID: "2", userID: "mHattrup" },
-//   { photoID: "2", userID: "sSchmidt" }
-// ];
 const requestGithubToken = credentials =>
   fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
@@ -86,6 +47,15 @@ module.exports = {
     me: (parent, args, { currentUser }) => currentUser
   },
   Mutation: {
+    dropUsers: async (parent, args, { db }) => {
+      const result = await db.collection("users").drop((err, delOK) => {
+        if (err) throw err;
+        if (delOK) {
+          console.log("Users Deleted.");
+          return true;
+        }
+      });
+    },
     fakeUserAuth: async (parent, { githubLogin }, { db }) => {
       const user = await db.collection("users").findOne({ githubLogin });
       if (!user) {
